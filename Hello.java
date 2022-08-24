681230680d4e79a7a93bfc27611cac29f1ac06c2
HvnterSmith <112022753+HvnterSmith@users.noreply.github.com>
2022-08-24 16:20:42 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/********************************
+* Hello.java
+*********************************
+* Hunter Smith
+* 8/24/2022
+* CMSC-255-001
+*********************************/
+
+public class Hello {
+	public static void main(String[] args){
+
+		System.out.println("Hunter Smith");
+		System.out.println("Hello");
+		System.out.println("Simple output program");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC-255-001");
+		System.out.println();
+
+
+	}
+}
